d8e6122136e02674114efb12390186578e1a37b7
Karen V <karen.pretzel.365@gmail.com>
2021-08-08 11:22:30 -0500

    Revert "Revert "the bmi is calculated and displayed with a background color""
    This reverts commit 8169143859d2133517ce9861e2f3dabdf66224fb.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
+var cal = 0;
+
 var arrayOfFilters = [];
 var arrayOfCautions = [];
 var otherHtml = "./secondPage.html";
-
 
 var calculateBtn = $("#calculateBtn");
 var searchInputBar = $('input[name = "input"]');
@@ -9,7 +10,7 @@ var heightInput = $("#heightInput");
 var weightInput = $("#weightInput");
 var ageInput = $("#ageInput");
 var genderInput = $("#genderDiv");
-
+var calculatedBmi = $("#bmi-Category")
 
 heightInput.val(localStorage.getItem("height"));
 weightInput.val(localStorage.getItem("weight"));
@@ -51,6 +52,7 @@ function updateUserInfo() {
   userInput.gender = localStorage.getItem("gender");
   console.log(userInput.gender);
 }
+
 $("#submit").on("click", function () {
   updateUserInfo();
   console.log(userInput);
@@ -75,7 +77,8 @@ $("#submit").on("click", function () {
   //console.log(arrayOfFilters.join(""));
   userInput.search = localStorage.getItem("searchInput");
   console.log(userInput);
-  getRecipesByCalorie(userInput); //calling the first function
+  //getRecipesByCalorie(userInput); //calling the first function
+  getRecipes(userInput, cal); //3 meals
 });
 
 // ***** RETRIEVE DATA ACCORDING TO USER'S NEEDS *****
@@ -89,8 +92,36 @@ let userInput = {
   search: "",
 };
 
+function calculateBMI() {
+  var weight = userInput.weight;
+  
+  console.log("userINput " + weight);
+
+  var height = userInput.height;
+  
+  let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  console.log(bmi)
+
+  if (bmi < 18.6) {
+    console.log("underweight")
+    calculatedBmi.text("Underweight")
+    calculatedBmi.addClass("underWeight")
+  }
+  else if (bmi >= 18.6 && bmi < 24.9){
+    console.log("normal")
+    calculatedBmi.text("Normal")
+    calculatedBmi.addClass("normalWeight")
+  }else {
+    console.log("overweight")
+    calculatedBmi.text("Overweight")
+    calculatedBmi.addClass("overWeight")
+  };
+}
+
 // Start the API Call to retrieve all recipies filtered by calories
-//getRecipesByCalorie(userInput);
+updateUserInfo();
+getRecipesByCalorie(userInput);
+calculateBMI();
 
 // ***** MANAGE API CALL TO FITNESS API TO GET CALORIE INTAKE FOR USER *****
 
@@ -131,11 +162,12 @@ function getRecipesByCalorie(userInput) {
     // ********************
     // ********************
     console.log(response); // these are the calories for user display these in page
-    getRecipes(userInput, response.calorie / 3); //3 meals
-  });
-  
-}
+    $("#calories").text(Math.round(response.calorie / 3));
 
+    // getRecipes(userInput, response.calorie / 3); //3 meals
+    cal = response.calorie / 3;
+  });
+}
 
 // ***** MANAGE API CALL TO FOOD API TO GET RECIPES *****
 
@@ -171,7 +203,6 @@ function getRecipes(userInput, calorie) {
 function processRecipes(recipes, calorie) {
   var validRecipes = [];
   console.log("max allowed " + calorie);
- 
 
   for (let i = 0; i < recipes.length; i++) {
     var valid = true;
@@ -210,7 +241,7 @@ function processRecipes(recipes, calorie) {
   showRecipes(validRecipes); //call the showRecipes function
 }
 
-
+//my stuff
 
 var recipeDiv = $("#recipeDiv");
 
